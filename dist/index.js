7e9919d3579d9d4daf4c85a0d883f93055564ba1
sukreshmanda <34400639+sukreshmanda@users.noreply.github.com>
2020-12-04 12:09:13 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -14,6 +14,7 @@ fs.readFile('list.conf', 'utf8' , function (err,data) {
     let text = data.substring(0,data.indexOf("https://"))
     let link = data.substring(data.indexOf("https://"),data.length)
     let source = "<img src=\"https://cdn1.iconfinder.com/data/icons/web-page-and-iternet/90/Web_page_and_internet_icons-04-512.png\" width=\"22px\"> <a href=\""+link+"\">"+text+" </a> <br />"
+    console.log(source)
     returning = returning+source
   }
   fs.readFile('README.md','utf8', function (errr,dataa){
