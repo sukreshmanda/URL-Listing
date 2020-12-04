6eae2a91194317ad04ca67e5858bc9ea8730c842
sukreshmanda <34400639+sukreshmanda@users.noreply.github.com>
2020-12-04 10:16:16 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,9 @@ fs.readFile('list.conf', 'utf8' , function (err,data) {
   let i=0
   for(i;i<array.length;i++){
     console.log(array[i])
-    let link = array[i]
+    let data = array[i]
+    let text = data.substring(0,data.indexOf("https://"))
+    let link = data.substring(data.indexOf("https://"),data.length)
+    console.log("text: "+text+" link: "+link)
   }
 });
